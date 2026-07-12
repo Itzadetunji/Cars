7952e03859d3a94120d14728530b0620342a64a5
itzadetunji <adetunjiadeyinka29@gmail.com>
2026-07-13 00:34:36 +0100

    Add asset files for stamps and Toyota images, including SVG and JPG formats, and update CarsListItem view to incorporate new images in the UI.

--- ios/Cars/Views/CarsLIstView/CarsListItem.swift
+++ ios/Cars/Views/CarsLIstView/CarsListItem.swift
@@ -11,25 +11,43 @@ struct CarsListItem: View {
     @Environment(\.colorScheme) private var colorScheme
 
     var body: some View {
+
         Button {
-            
+
         } label: {
-            Text("Hello World").font(SofiaFont.black(size: 16))
-                .frame(maxWidth: .infinity, minHeight: 100)
-                
+            VStack(spacing: 0) {
+
+                ZStack {
+                    Image(.stamp1)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(.foreground)
+                    
+                    Image(.toyota)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(.foreground)
+                        .padding(10)
+                }
+                Text("Hello World").font(SofiaFont.black(size: 16)).padding(.vertical).foregroundStyle(.foreground)
+
+            }
         }
-        .buttonStyle(.plain)
-        
-        .background(Color(.systemBackground))
-        .cornerRadius(16)
-        .shadow(
-            color: (colorScheme == .dark ? Color.white : Color.black).opacity(0.12),
-            radius: 2,
-            x: 0,
-            y: 2
-        )
+        //        .buttonStyle(.plain)
+
+        //        .background(Color(.systemBackground))
+        //        .cornerRadius(16)
+        //        .shadow(
+        //            color: (colorScheme == .dark ? Color.white : Color.black).opacity(0.12),
+        //            radius: 2,
+        //            x: 0,
+        //            y: 2
+        //        )
         .padding(.horizontal, 3)
-            
+
     }
 }
 
